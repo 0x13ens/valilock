d766014324aa0967733c2b7843ae9e792b754737
Ben <core@bens-mbp.home>
2020-01-24 21:18:55 +0000

    first setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Valilock
                 </div>
 
                 <div class="links">
