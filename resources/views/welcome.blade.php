d3a4db6f49b330d768f4ffb8d39f966e6abe0c48
Ben <core@bens-mbp.home>
2020-01-24 23:58:41 +0000

    updated title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Landrick - Saas & Software Landing Page Template</title>
+        Online payment processing for internet businesses - Stripe
+        <title>Dynamic Password Processing for internet businesses - Valilock  </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Premium Bootstrap 4 Landing Page Template" />
         <meta name="keywords" content="bootstrap 4, premium, marketing, multipurpose" />
